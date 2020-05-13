de17331e0aff50d2f50b93e73f0c0f769e2c0516
Matthew Mackey <matthewmackey@users.noreply.github.com>
2020-05-12 22:14:34 -0400

    Moved code in install.sh to functions

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Base16 terminal colorscheme manager (also configures ~/.vimrc_background)
-if [[ ! -d ~/.config/base16-shell/.git ]]; then
-  git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-else
-  echo "Base16 Shell already installed"
-fi
+install_base16_shell() {
+    # Base16 terminal colorscheme manager (also configures ~/.vimrc_background)
+    if [[ ! -d ~/.config/base16-shell/.git ]]; then
+      git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+    else
+      echo "Base16 Shell already installed"
+    fi
+}
 
-if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-else
-  echo "Vundle already installed"
-fi
+install_vim_vundle() {
+    if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
+      git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    else
+      echo "Vundle already installed"
+    fi
+}
+
+install_base16_shell
+install_vim_vundle
