0ff13eff16cfda3011baf6d3825cc162164bd4ef
Matthew Mackey <matthewmackey@users.noreply.github.com>
2020-05-15 12:17:16 -0400

    desktop: added .desktop files from Chrome profiles

--- install.sh
+++ install.sh
@@ -17,5 +17,18 @@ install_vim_vundle() {
     fi
 }
 
+# PLACEHOLDER function: didn't have time to figure out where to store the
+# Chrome profile icons b/c didn't want to store them in git; in the future,
+# when I have storage for them I can use this method to deploy them
+#
+# The .gifs I have been using are my Gmail profile pics (hence -gmail.gif)
+copy_chrome_icons() {
+  for i in mm mackey
+  do
+    img_filename=chrome_${i}_profile-gmail.gif
+    cp $img_filename ~/.local/share/applications/$img_filename
+  done
+}
+
 install_base16_shell
 install_vim_vundle
--- stow.sh
+++ stow.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in bash git mmbin ssh tmux vim; do
+for i in bash desktop git mmbin ssh tmux vim; do
   echo "Running stow for -> [$i]"
   stow $i
 done
