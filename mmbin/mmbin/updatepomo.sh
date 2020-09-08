467e11959db99c8860029704989de849053fd12a
Matthew Mackey <matthewmackey@users.noreply.github.com>
2020-09-08 11:11:45 -0400

    pomodoro: script to set Pomordoro timer duration

---
+++ mmbin/mmbin/updatepomo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONFIG_IN=~/.cinnamon/configs/pomodoro@gregfreeman.org/pomodoro@gregfreeman.org.json.orig
+CONFIG_OUT=~/.cinnamon/configs/pomodoro@gregfreeman.org/pomodoro@gregfreeman.org.json
+
+NEW_DURATION_MINS=$1
+
+if [ -z "$NEW_DURATION_MINS" ]; then
+  CURR_DURATION_MINS=$(cat $CONFIG_OUT | jq '.pomodoro_duration.value')
+  read -p "Enter new pomododo duration (current [$CURR_DURATION_MINS min]): " NEW_DURATION_MINS
+fi
+
+cat $CONFIG_IN | jq 'setpath(["pomodoro_duration", "value"]; '$NEW_DURATION_MINS')' >$CONFIG_OUT
