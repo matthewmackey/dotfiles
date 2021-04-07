141b3a40cfeea6f755841bff9f109232b2c75e78
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2021-04-07 10:52:28 -0400

    pomo: fix script so it won't overwrite other settings

--- mmbin/mmbin/pomodoroupdate.sh
+++ mmbin/mmbin/pomodoroupdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CONFIG_IN=~/.cinnamon/configs/pomodoro@gregfreeman.org/pomodoro@gregfreeman.org.json.orig
+#CONFIG_IN=~/.cinnamon/configs/pomodoro@gregfreeman.org/pomodoro@gregfreeman.org.json.orig
+CONFIG_IN=~/.cinnamon/configs/pomodoro@gregfreeman.org/pomodoro@gregfreeman.org.json
 CONFIG_OUT=~/.cinnamon/configs/pomodoro@gregfreeman.org/pomodoro@gregfreeman.org.json
 
 NEW_DURATION_MINS=$1
@@ -10,4 +11,5 @@ if [ -z "$NEW_DURATION_MINS" ]; then
   read -p "Enter new pomododo duration (current [$CURR_DURATION_MINS min]): " NEW_DURATION_MINS
 fi
 
-cat $CONFIG_IN | jq 'setpath(["pomodoro_duration", "value"]; '$NEW_DURATION_MINS')' >$CONFIG_OUT
+NEW_CONFIG=$(cat $CONFIG_IN | jq 'setpath(["pomodoro_duration", "value"]; '$NEW_DURATION_MINS')')
+echo "$NEW_CONFIG" >$CONFIG_OUT
