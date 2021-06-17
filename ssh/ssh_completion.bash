424321357b38f3d25573845c7f60d096bfd85f09
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-06-17 17:02:49 -0400

    ssh: moved bash completion to ssh folder

---
+++ ssh/ssh_completion.bash
@@ -0,0 +1,8 @@
+#---------------------------#
+# COMPLETION - SSH          #
+#---------------------------#
+# Will autocomplete any hosts found in known_hosts
+# From: https://gist.github.com/peelman/1773863
+complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
+
+
