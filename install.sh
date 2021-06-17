9bb01a87e7550685003de9b5c51b9430cbc60f41
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-06-17 16:33:11 -0400

    install.sh - create backup of .bashrc

--- install.sh
+++ install.sh
@@ -8,16 +8,28 @@ msg() {
 # SYMLINK - .bashrc             #
 #-------------------------------#
 if [ ! -e ~/.bashrc ]; then
+
   msg ".bashrc does not exist -> creating symlink"
   ln -s ~/dotfiles/.bashrc ~/.bashrc
+
 elif [ -L ~/.bashrc ]; then
   msg ".bashrc exists AND is symlink"
+
   msg "Deleting $(ls -al ~/.bashrc)"
   rm ~/.bashrc
-  msg "Creating .bashrc symlink"
+
   ln -s ~/dotfiles/.bashrc ~/.bashrc
+  msg "Created.bashrc symlink"
+
 else
   msg ".bashrc exists AND is NOT a symlink"
+  bashrc_backup=~/.bashrc.bak."$(date +%F_%R)"
+
+  msg "Moving existing .bashrc to -> [$bashrc_backup]"
+  mv ~/.bashrc "$bashrc_backup"
+
+  ln -s ~/dotfiles/.bashrc ~/.bashrc
+  msg "Created.bashrc symlink"
 fi
 
 
