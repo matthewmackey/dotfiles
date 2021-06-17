ddae4830b01f99e6edc19a6c14e8aa971cf9ba8a
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-06-17 16:51:14 -0400

    Fixed git install.sh symlinks

--- git/install.sh
+++ git/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ln -s ~/.gitconfig ~/dotfiles/git/.gitconfig
-ln -s ~/.gitignore_global ~/dotfiles/git/.gitignore_global
-ln -s ~/.gitmessage.txt ~/dotfiles/git/.gitmessage.txt
+ln -s ~/dotfiles/git/.gitconfig        ~/.gitconfig
+ln -s ~/dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/git/.gitmessage.txt   ~/.gitmessage.txt
