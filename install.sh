2bba512504405b1a3044fd57f4af9cd10a690328
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-06-17 16:30:02 -0400

    Test updates to install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+msg() {
+  printf "%s\n" "$1"
+}
+
 #-------------------------------#
 # SYMLINK - .bashrc             #
 #-------------------------------#
-if [[ ! -L ~/.bashrc ]]; then
+if [ ! -e ~/.bashrc ]; then
+  msg ".bashrc does not exist -> creating symlink"
   ln -s ~/dotfiles/.bashrc ~/.bashrc
+elif [ -L ~/.bashrc ]; then
+  msg ".bashrc exists AND is symlink"
+  msg "Deleting $(ls -al ~/.bashrc)"
+  rm ~/.bashrc
+  msg "Creating .bashrc symlink"
+  ln -s ~/dotfiles/.bashrc ~/.bashrc
+else
+  msg ".bashrc exists AND is NOT a symlink"
 fi
 
 
-./ssh/install.sh
-./tmux/install.sh
-./vim/install.sh
+#./ssh/install.sh
+#./tmux/install.sh
+#./vim/install.sh
