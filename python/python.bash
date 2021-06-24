e5b086d50eaca63c8f9c5b80acef57daad3b1eda
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-06-24 11:35:21 -0400

    python: add default python to PATH always if exist

--- python/python.bash
+++ python/python.bash
@@ -1,15 +1,18 @@
-#----------------------------------------------------------------------------
-# Virtualenvwrapper setup
-#----------------------------------------------------------------------------
 default_python_bin=~/pythons/default/bin
-if [[ -f $default_python_bin/virtualenvwrapper.sh ]]; then
+
+# Add Default Python BIN directory w/ python/pip & base modules to PATH
+if [ -d "$default_python_bin" ]; then
+  export PATH=$default_python_bin:$PATH
+fi
+
+#--------------------------#
+# Virtualenvwrapper setup
+#--------------------------#
+if [ -f $default_python_bin/virtualenvwrapper.sh ]; then
   export WORKON_HOME=$HOME/virtualenvs
   export VIRTUALENVWRAPPER_PYTHON=$default_python_bin/python
   export VIRTUALENVWRAPPER_VIRTUALENV=$default_python_bin/virtualenv
   export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--always-copy'
   source $default_python_bin/virtualenvwrapper.sh
 
-  # Add Default Python BIN directory w/ python/pip & base modules to PATH
-  export PATH=$default_python_bin:$PATH
 fi
-
