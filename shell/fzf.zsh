c2d690baef76dd8b72136b214f3c3bb9a1a3955c
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-07-21 09:03:24 -0400

    shell: removed hardcoded HOME path from shell configs

--- shell/fzf.zsh
+++ shell/fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/mmackey/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/mmackey/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/mmackey/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/mmackey/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
