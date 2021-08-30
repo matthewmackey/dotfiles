79811ca8ce7598c09a78474ba0e077cdb1134161
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-08-30 16:37:00 -0400

    shell: install yh, kubectx, kubens utilities in pre-install

--- shell/install.sh
+++ shell/install.sh
@@ -4,3 +4,18 @@
 if [ ! -d ~/.oh-my-zsh ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+# Install `yh` YAML formatter
+go get github.com/andreazorzetto/yh
+
+# Install kubectx
+if [ ! -f ~/bin/kubectx -a ! -f ~/bin/kubens ]; then
+  curl -L https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_x86_64.tar.gz -o /tmp/kubectx-0.9.4.tar.gz
+  curl -L https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_linux_x86_64.tar.gz -o /tmp/kubens-0.9.4.tar.gz
+  {
+    cd ~/bin
+    tar xzf /tmp/kubectx-0.9.4.tar.gz
+    tar xzf /tmp/kubens-0.9.4.tar.gz
+    rm LICENSE
+  }
+fi
