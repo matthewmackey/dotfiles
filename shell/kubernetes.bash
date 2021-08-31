f1dd5a47094ab1307dfd26df3d9ebf40c7739b7c
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-08-30 21:06:27 -0400

    shell: use kube-ps1 4 k8s context in bash/zsh prompt

--- shell/kubernetes.bash
+++ shell/kubernetes.bash
@@ -1,3 +1,10 @@
 [ $(command -v kubectl) ] && source <(kubectl completion bash)
 alias k=kubectl
 complete -F __start_kubectl k
+
+if [ ! -f ~/.local/bin/kube-ps1.sh ]; then
+  curl -L https://raw.githubusercontent.com/jonmosco/kube-ps1/v0.7.0/kube-ps1.sh \
+    -o ~/.local/bin/kube-ps1.sh
+fi
+source ~/.local/bin/kube-ps1.sh
+PS1='$(kube_ps1) '$PS1
