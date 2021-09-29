88f0314f5e0b4a12244d1a59e8a81509cc95e720
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-09-29 15:15:17 -0400

    shell: removed source of kubectl ZSH completions

--- shell/kubernetes.zsh
+++ shell/kubernetes.zsh
@@ -1,4 +1,5 @@
-[[ $commands[kubectl] ]] && source <(kubectl completion zsh)
+# Following line NOT needed if using oh-my-zsh 'kubectl' plugin:
+# [[ $commands[kubectl] ]] && source <(kubectl completion zsh)
 
 if [ ! -f ~/.local/bin/kube-ps1.sh ]; then
   curl -L https://raw.githubusercontent.com/jonmosco/kube-ps1/v0.7.0/kube-ps1.sh \
