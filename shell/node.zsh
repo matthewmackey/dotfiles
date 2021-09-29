cce921627c2b2b608f943b72c1ae7a1495b187a2
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-09-29 15:20:30 -0400

    shell: now lazy load NVM in ZSH

--- shell/node.zsh
+++ shell/node.zsh
@@ -1,2 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Create alias to lazy load NVM to keep shell load time down
+# This loads NVM
+alias snvm='[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"'
