ad7e02b98d6647f02e271d0c881c818901a12821
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-11-19 14:05:56 -0500

    vim: updated nvim install script

--- vim/install_nvim_nightly.sh
+++ vim/install_nvim_nightly.sh
@@ -1,27 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
-DOWNLOAD_FILE=~/downloads/srv/nvim.appimage
+set -e
+set -o pipefail
+
+DOWNLOADS_DIR=~/downloads/srv
+CURR_NIGHTLY_DOWNLOAD_FILE=$DOWNLOADS_DIR/nvim.appimage.tmp
 NIGHTLY_URL=https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 
-if [ -e "${DOWNLOAD_FILE}" ]; then
-  printf "nvim already downloaded at [${DOWNLOAD_FILE}]; delete then re-run\n"
-  exit 1
-fi
+mkdir -p $DOWNLOADS_DIR 2>/dev/null || true
 
-curl -L -o "${DOWNLOAD_FILE}" "${NIGHTLY_URL}"
-chmod +x "${DOWNLOAD_FILE}"
-printf "\nDownloaded: [${DOWNLOAD_FILE}]\n"
+remove_last_download_if_exists() {
+  if [ -e "${CURR_NIGHTLY_DOWNLOAD_FILE}" ]; then
+    rm ${CURR_NIGHTLY_DOWNLOAD_FILE}
+    printf "Deleting last download of [${CURR_NIGHTLY_DOWNLOAD_FILE}]\n"
+  fi
+}
 
-nvim_nightly_version=$("${DOWNLOAD_FILE}" --version | head -n1 | cut -d ' ' -f2)
-printf "Nightly Version downloaded: [${nvim_nightly_version}]\n"
+download_current_nightly_version() {
+  printf "\nDownloading: [${NIGHTLY_URL}]\n\n"
+  curl -L -o "${CURR_NIGHTLY_DOWNLOAD_FILE}" "${NIGHTLY_URL}"
+  chmod +x "${CURR_NIGHTLY_DOWNLOAD_FILE}"
+  printf "\nDownloaded: [${CURR_NIGHTLY_DOWNLOAD_FILE}]\n\n"
+}
 
-versioned_download_file=~/downloads/srv/nvim-"${nvim_nightly_version}.appimage"
-rm "${versioned_download_file}" 2>/dev/null
-ln -s "${DOWNLOAD_FILE}" "${versioned_download_file}"
-printf "\nCreated versioned symlink at:\n  "
-ls -l --color=always "${versioned_download_file}"
+get_current_nightly_version_number() {
+  "${CURR_NIGHTLY_DOWNLOAD_FILE}" --version | head -n1 | cut -d ' ' -f2
+}
 
-sudo rm /usr/bin/nvim 2>/dev/null
-sudo ln -s "${versioned_download_file}" /usr/bin/nvim
-printf "\nCreated symlink at:\n  "
-ls -l --color=always /usr/bin/nvim
+rename_nightly_version_to_real_version_number() {
+  printf "Nightly Version downloaded: [${NVIM_NIGHTLY_VERSION}]\n"
+  versioned_download_file=~/downloads/srv/nvim-"${NVIM_NIGHTLY_VERSION}.appimage"
+
+  if [ ! -e "${versioned_download_file}" ]; then
+    mv "${CURR_NIGHTLY_DOWNLOAD_FILE}" "${versioned_download_file}"
+    ls -l --color=always "${versioned_download_file}"
+  else
+    printf "Nightly version already existed and installed\nDONE\n"
+    exit 0
+  fi
+}
+
+symlink_global_nvim_to_nightly_version() {
+  sudo rm /usr/bin/nvim 2>/dev/null || true
+  sudo ln -s "${versioned_download_file}" /usr/bin/nvim
+  printf "\nCreated symlink at:\n  "
+  ls -l --color=always /usr/bin/nvim
+}
+
+remove_last_download_if_exists
+download_current_nightly_version
+NVIM_NIGHTLY_VERSION=$(get_current_nightly_version_number)
+rename_nightly_version_to_real_version_number
+symlink_global_nvim_to_nightly_version
+
