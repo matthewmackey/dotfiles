11df86da1013b3bc4835d8484a2eb982d2a405ce
Matthew Machaj <73896224+matthewmachaj@users.noreply.github.com>
2021-12-01 16:33:07 -0500

    xmodmap: add files for switching CapsLock/Esc
    Also added aliases for switching and added folds to the aliases file.

--- xmodmap/install.sh
+++ xmodmap/install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 DIR=~/.config/xmodmap
-FILE=.xmodmap-esc
 
-if [ ! -d $DIR ]; then
-  mkdir -p $DIR
+if [ ! -L $DIR ]; then
+  ln -s ~/dotfiles/xmodmap $DIR
 fi
 
-ln -s ~/dotfiles/xmodmap/$FILE $DIR/$FILE
