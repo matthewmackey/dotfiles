297e08454e7613b4f026b653abe7be39040dd9e8
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-02-14 17:04:22 -0500

    desktop: add Synapse .desktop file to autostart directory

--- desktop/install.sh
+++ desktop/install.sh
@@ -7,3 +7,6 @@ source $DOTDIR/lib/common.sh
 
 print_step "Linking static .desktop files into ~/.local"
 create_symlink_with_backup $DOTDIR/desktop ~/.local/share/applications/personal
+
+print_step "Linking autostart .desktop files into ~/.config/autostart"
+create_symlink_with_backup $DOTDIR/desktop/synapse.desktop ~/.config/autostart/synapse.desktop
