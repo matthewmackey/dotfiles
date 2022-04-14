94b777ee68ffb51538193436e399f5353c2680bd
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-04-13 23:08:59 -0400

    sh/node: install 'nvm' if it is not installed already on shell init

--- .config/sh/includes/node.sh
+++ .config/sh/includes/node.sh
@@ -1,7 +1,11 @@
 #
 # node.sh
 #
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR=$HOME/.nvm
+
+if [ ! -f $NVM_DIR/nvm.sh ]; then
+   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+fi
 
 # Create alias to lazy load NVM to keep shell load time down
 # This loads NVM
