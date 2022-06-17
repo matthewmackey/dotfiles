38c754d38c954a9ce4ea11fbc5c5495b8e56a430
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-06-17 14:30:04 -0400

    sh/kubectl: active GKE gcloud auth plugin

--- .config/sh/includes/kubectl.sh
+++ .config/sh/includes/kubectl.sh
@@ -1,3 +1,6 @@
 if [ -d ~/.krew ]; then
   export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 fi
+
+# https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
