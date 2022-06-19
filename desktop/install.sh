dbbbdec455ab51174998f74bc6be30d0aa9306ba
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-06-19 00:17:38 -0400

    desktop: disable Gnome SSH agent via Autostart

--- desktop/install.sh
+++ desktop/install.sh
@@ -10,3 +10,4 @@ create_symlink_with_backup $DOTDIR/desktop ~/.local/share/applications/personal
 
 print_step "Linking autostart .desktop files into ~/.config/autostart"
 create_symlink_with_backup $DOTDIR/desktop/synapse.desktop ~/.config/autostart/synapse.desktop
+create_symlink_with_backup $DOTDIR/desktop/disable-gnome-ssh-agent.desktop ~/.config/autostart/gnome-keyring-ssh.desktop
