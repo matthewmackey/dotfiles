ec83c5777a2044c2b38d865bf6535a14069a49e5
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-06-19 00:18:43 -0400

    zsh/aliases: updated aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -56,6 +56,8 @@ background() {
 # Global Aliases                   #
 #----------------------------------#
 alias -g B='| base64 -d'
+alias -g BE='| base64 --wrap=0'
+alias -g C='| xclip -sel clip /dev/stdin'
 alias -g G='| grep'
 alias -g J='| jq'
 alias -g L='| less'
