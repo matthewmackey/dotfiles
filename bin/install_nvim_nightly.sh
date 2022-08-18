07a96860ce292744ab84a624749ebd4588aff597
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-08-18 15:28:47 -0700

    nvim_install: now update /usr/bin/{vi,vim} symlinks

--- bin/install_nvim_nightly.sh
+++ bin/install_nvim_nightly.sh
@@ -7,6 +7,8 @@ DOWNLOADS_DIR=~/downloads/srv
 CURR_NIGHTLY_DOWNLOAD_FILE=$DOWNLOADS_DIR/nvim.appimage.tmp
 NIGHTLY_URL=https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 
+_VERSIONED_DOWNLOAD_FILE=
+
 mkdir -p $DOWNLOADS_DIR 2>/dev/null || true
 
 remove_last_download_if_exists() {
@@ -29,11 +31,11 @@ get_current_nightly_version_number() {
 
 rename_nightly_version_to_real_version_number() {
   printf "Nightly Version downloaded: [${NVIM_NIGHTLY_VERSION}]\n"
-  versioned_download_file=~/downloads/srv/nvim-"${NVIM_NIGHTLY_VERSION}.appimage"
+  _VERSIONED_DOWNLOAD_FILE=~/downloads/srv/nvim-"${NVIM_NIGHTLY_VERSION}.appimage"
 
-  if [ ! -e "${versioned_download_file}" ]; then
-    mv "${CURR_NIGHTLY_DOWNLOAD_FILE}" "${versioned_download_file}"
-    ls -l --color=always "${versioned_download_file}"
+  if [ ! -e "${_VERSIONED_DOWNLOAD_FILE}" ]; then
+    mv "${CURR_NIGHTLY_DOWNLOAD_FILE}" "${_VERSIONED_DOWNLOAD_FILE}"
+    ls -l --color=always "${_VERSIONED_DOWNLOAD_FILE}"
   else
     printf "Nightly version already existed and installed\nDONE\n"
     exit 0
@@ -41,15 +43,33 @@ rename_nightly_version_to_real_version_number() {
 }
 
 symlink_global_nvim_to_nightly_version() {
-  sudo rm /usr/bin/nvim 2>/dev/null || true
-  sudo ln -s "${versioned_download_file}" /usr/bin/nvim
-  printf "\nCreated symlink at:\n  "
-  ls -l --color=always /usr/bin/nvim
+  replace_global_symlink /usr/bin/nvim "${_VERSIONED_DOWNLOAD_FILE}"
 }
+
+symlink_global_vim_to_nvim() {
+  replace_global_symlink /usr/bin/vim /usr/bin/nvim
+}
+
+symlink_global_vi_to_nvim() {
+  replace_global_symlink /usr/bin/vi /usr/bin/nvim
+}
+
+replace_global_symlink() {
+  local _link=$1
+  local _new_target=$2
+
+  sudo rm $_link 2>/dev/null || true
+  sudo ln -s $_new_target $_link
+  printf "\nCreated symlink at:\n  "
+  ls -l --color=always $_link
+}
+
+
 
 remove_last_download_if_exists
 download_current_nightly_version
 NVIM_NIGHTLY_VERSION=$(get_current_nightly_version_number)
 rename_nightly_version_to_real_version_number
 symlink_global_nvim_to_nightly_version
-
+symlink_global_vim_to_nvim
+symlink_global_vi_to_nvim
