0241c1435a962a30c0cc9cf79b7fef578bddd24e
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2022-11-17 13:21:14 -0500

    Updated asdf install script to ensure ASDF_HOME is loaded

--- post-install/install-asdf.sh
+++ post-install/install-asdf.sh
@@ -5,17 +5,39 @@ set -o pipefail
 
 ASDF_VERSION=v0.10.2
 ASDF_PLUGIN_HOME=$ASDF_HOME/plugins
+APT_CACHE_LAST_UPDATE_MINS=60
 
-# if [ ! -d ${ASDF_HOME} ]; then
-  git clone https://github.com/asdf-vm/asdf.git ${ASDF_HOME} --branch ${ASDF_VERSION}
-# fi
+source $DOTDIR/lib/common.sh
+
+# Ensure ASDF_HOME is in environment in case this script is running immediately
+# after dotfiles were installed and shell hasn't been reloaded
+source $DOTDIR/.config/sh/includes/asdf.sh
+
+section "Installing asdf"
+
+print_step "Cloning asdf repository to $ASDF_HOME"
+if [ ! -d ${ASDF_HOME} ]; then
+  git clone https://github.com/asdf-vm/asdf.git $ASDF_HOME} --branch $ASDF_VERSION
+else
+  skipping "asdf repository has already been cloned to $ASDF_HOME-> asdf is considered already installed"
+fi
 
 # Enable `asdf` to get access to `asdf plugin` commands
 source $ASDF_HOME/asdf.sh
 
-sudo apt-get update
+section "Installing asdf Plugins"
+
+print_step "Refreshing APT cache"
+# See: https://unix.stackexchange.com/questions/271000/update-apt-get-cache-only-if-its-older-than-certain-time
+# See: https://viewsby.wordpress.com/2012/08/24/linux-find-mmin-minutes/
+if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -$APT_CACHE_LAST_UPDATE_MINS)" ]; then
+  sudo apt-get update
+else
+  skipping "APT cache was updated within last $APT_CACHE_LAST_UPDATE_MINS minutes"
+fi
 
 # NODE.JS
+print_step "Installing asdf [node.js] plugin"
 if [ ! -d $ASDF_PLUGIN_HOME/nodejs ]; then
   # Install Node.js plugin pre-requisites
   #   https://github.com/asdf-vm/asdf-nodejs
@@ -27,9 +49,12 @@ if [ ! -d $ASDF_PLUGIN_HOME/nodejs ]; then
     python3-pip
   asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || \
   echo "[ERROR] installing Node.js plugin"
+else
+  skipping "asdf [node.js] plugin is already installed"
 fi
 
 # PYTHON
+print_step "Installing asdf [python] plugin"
 if [ ! -d $ASDF_PLUGIN_HOME/python ]; then
   # Install Python plugin pre-requisites
   #   https://github.com/asdf-community/asdf-python
@@ -54,9 +79,12 @@ if [ ! -d $ASDF_PLUGIN_HOME/python ]; then
     zlib1g-dev && \
   asdf plugin add python https://github.com/asdf-community/asdf-python.git || \
   echo "[ERROR] installing Python plugin"
+else
+  skipping "asdf [python] plugin is already installed"
 fi
 
 # RUBY
+print_step "Installing asdf [ruby] plugin"
 if [ ! -d $ASDF_PLUGIN_HOME/ruby ]; then
   # Install Ruby plugin pre-requisites
   #   https://github.com/asdf-vm/asdf-ruby
@@ -77,4 +105,6 @@ if [ ! -d $ASDF_PLUGIN_HOME/ruby ]; then
     zlib1g-dev && \
   asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git || \
   echo "[ERROR] installing Ruby plugin"
+else
+  skipping "asdf [ruby] plugin is already installed"
 fi
