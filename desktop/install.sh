3e43e5479e1f374b3e317b4e0e8f04853fda6411
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2022-12-06 22:13:45 -0500

    kensignton: add script to setup Kensington trackball on Startup

--- desktop/install.sh
+++ desktop/install.sh
@@ -10,4 +10,5 @@ create_symlink_with_backup $DOTDIR/desktop ~/.local/share/applications/personal
 
 print_step "Linking autostart .desktop files into ~/.config/autostart"
 create_symlink_with_backup $DOTDIR/desktop/synapse.desktop ~/.config/autostart/synapse.desktop
+create_symlink_with_backup $DOTDIR/desktop/kensington-trackball.desktop ~/.config/autostart/kensington-trackball.desktop
 create_symlink_with_backup $DOTDIR/desktop/disable-gnome-ssh-agent.desktop ~/.config/autostart/gnome-keyring-ssh.desktop
