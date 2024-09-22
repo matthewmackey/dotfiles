90dc1a2d41b9276e2103fd1c93f239983a46b722
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2024-09-22 13:04:28 -0400

    gui/vscode: ensure ~/.config/Code/User exists

--- gui/vscode/install.sh
+++ gui/vscode/install.sh
@@ -5,12 +5,13 @@ set -o pipefail
 
 source $DOTDIR/lib/common.sh
 
-print_step "Setup VSCode Settings"
-
-# Map personal VS Code dotdir into ~/.config/vscode
 VSCODE_DOTDIR=~/.config/vscode
-create_symlink_with_backup $DOTDIR/gui/vscode $VSCODE_DOTDIR
-
 VSCODE_REAL_DIR=~/.config/Code/User
+
+print_step "Ensure $VSCODE_REAL_DIR directory exists"
+mkdir_if_not_exist $VSCODE_REAL_DIR
+
+print_step "Setup VSCode Settings"
+create_symlink_with_backup $DOTDIR/gui/vscode $VSCODE_DOTDIR
 create_symlink_with_backup $VSCODE_DOTDIR/keybindings.json $VSCODE_REAL_DIR/keybindings.json
 create_symlink_with_backup $VSCODE_DOTDIR/settings.json $VSCODE_REAL_DIR/settings.json
