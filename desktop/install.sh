c2fd9cf6037fb54768269d1d98d5694f34906ce1
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2024-09-22 13:04:06 -0400

    desktop/install: ensure ~/.config/autostart exists

--- desktop/install.sh
+++ desktop/install.sh
@@ -8,6 +8,9 @@ source $DOTDIR/lib/common.sh
 print_step "Linking static .desktop files into ~/.local"
 create_symlink_with_backup $DOTDIR/desktop ~/.local/share/applications/personal
 
+print_step "Ensuring ~/.config/autostart directory exists"
+mkdir_if_not_exist ~/.config/autostart
+
 print_step "Linking autostart .desktop files into ~/.config/autostart"
 create_symlink_with_backup $DOTDIR/desktop/synapse.desktop ~/.config/autostart/synapse.desktop
 create_symlink_with_backup $DOTDIR/desktop/kensington-trackball.desktop ~/.config/autostart/kensington-trackball.desktop
