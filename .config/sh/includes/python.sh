2cb8b4841bf24cdc6bf724f522f0ecdb1c6fbf5a
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2024-09-26 17:10:12 -0400

    python: moved most Python-related vars into main sh/env script b/c so important

--- .config/sh/includes/python.sh
+++ .config/sh/includes/python.sh
@@ -2,23 +2,7 @@
 # python.sh
 #
 
-export DEFAULT_PYTHON=$HOME/pythons/default
-
-_default_python_bin=$DEFAULT_PYTHON/bin
-
 # Add Default Python BIN directory w/ python/pip & base modules to PATH
-if [ -d "$_default_python_bin" ]; then
-  export PATH=$_default_python_bin:$PATH
+if [ -d "$DEFAULT_PYTHON_BIN_DIR" ]; then
+  export PATH="$DEFAULT_PYTHON_BIN_DIR:$PATH"
 fi
-
-#--------------------------#
-# Virtualenvwrapper setup
-#--------------------------#
-if [ -f $_default_python_bin/virtualenvwrapper.sh ]; then
-  export WORKON_HOME=$HOME/virtualenvs
-  export VIRTUALENVWRAPPER_PYTHON=$_default_python_bin/python
-  export VIRTUALENVWRAPPER_VIRTUALENV=$_default_python_bin/virtualenv
-  export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--always-copy'
-fi
-
-unset _default_python_bin
