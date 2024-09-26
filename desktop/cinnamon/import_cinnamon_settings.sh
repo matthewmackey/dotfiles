277716188700e5b21442829e0873ad47dcfa7c7b
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2024-09-26 16:03:47 -0400

    cinnamon: combined import/export; added All desktop confs

--- desktop/cinnamon/import_cinnamon_settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-EXPORT_DIR=$DOTDIR/desktop/cinnamon
-
-KEYBINDINGS_EXPORT=$EXPORT_DIR/keybindings.conf
-
-
-dconf load /org/cinnamon/desktop/keybindings/ < "$KEYBINDINGS_EXPORT"
---
+++ desktop/cinnamon/manage_cinnamon_settings.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# See: https://unix.stackexchange.com/questions/422111/linux-mint-cinnamon-18-in-what-file-are-the-keyboard-shortcuts-saved
+
+DCONF_PATH="/org/cinnamon/desktop/"
+
+EXPORT_DIR=$DOTDIR/desktop/cinnamon
+DESKTOP_EXPORT=$EXPORT_DIR/desktop.conf
+
+export_cinnamon() {
+  test ! -d "$EXPORT_DIR" && mkdir -p "$EXPORT_DIR"
+  dconf dump $DCONF_PATH > "$DESKTOP_EXPORT"
+}
+
+import_cinnamon() {
+  dconf load $DCONF_PATH < "$DESKTOP_EXPORT"
+}
+
+if [ "$1" = "-e" ]; then
+  export_cinnamon
+elif [ "$1" = "-i" ]; then
+  import_cinnamon
+else
+  echo "You must provide: [-i | -e]"
+  exit 1
+fi
