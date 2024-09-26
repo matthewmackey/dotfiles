5cfbda696bc77b35d5fbd83142cab02bb395238f
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2024-09-26 17:31:36 -0400

    python: got rid of DEFAULT_PYTHON in ~/virtualenvs concept

--- .config/sh/includes/python.sh
+++ .config/sh/includes/python.sh
@@ -3,6 +3,6 @@
 #
 
 # Add Default Python BIN directory w/ python/pip & base modules to PATH
-if [ -d "$DEFAULT_PYTHON_BIN_DIR" ]; then
-  export PATH="$DEFAULT_PYTHON_BIN_DIR:$PATH"
+if [ -d "$DEFAULT_ASDF_PYTHON_BIN_DIR" ]; then
+  export PATH="$DEFAULT_ASDF_PYTHON_BIN_DIR:$PATH"
 fi
